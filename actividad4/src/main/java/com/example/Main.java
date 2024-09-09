021c6fcc9ac168f5a3e7e9b31c5fa31efde76045
samuel <samuel08@gmail.com>
2024-09-09 18:24:01 -0500

    c1

--- actividad4/src/main/java/com/example/Main.java
+++ actividad4/src/main/java/com/example/Main.java
@@ -1,7 +1,26 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+ Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Ingresa el valor de la cuenta (incluyendo IVA): ");
+        double valorCuenta = scanner.nextDouble();
+        
+        System.out.print("Ingresa el porcentaje de propina que deseas dejar: ");
+        int porcentajePropina = scanner.nextInt();
+        
+        double valorPropina = valorCuenta * (porcentajePropina / 100.0);
+        
+        double valorTotal = valorCuenta + valorPropina;
+        
+        System.out.println("Valor de la cuenta: $" + String.format("%.2f", valorCuenta));
+        System.out.println("Porcentaje de propina: " + porcentajePropina + "%");
+        System.out.println("Valor de la propina: $" + String.format("%.2f", valorPropina));
+        System.out.println("Valor total a pagar: $" + String.format("%.2f", valorTotal));
+        
+        scanner.close();
     }
 }
